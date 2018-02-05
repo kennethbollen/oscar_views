3bef17c32ec017f3760202e323fce8ba7f583550
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-02-05 08:49:47 +0000

    Create data_extract.py

---
+++ data_extract.py
@@ -0,0 +1,16 @@
+import wikipedia
+
+oscar = wikipedia.page('Academy_Awards')
+
+oscar_shows = []
+
+#collect all the links for the annual oscar shows
+for i in oscar.links:
+    if re.search('.th Academy Awards', i) is not None:
+        oscar_shows.append(i)
+    elif re.search('.st Academy Awards', i) is not None:
+        oscar_shows.append(i)
+    elif re.search('.rd Academy Awards', i) is not None:
+        oscar_shows.append(i)
+    else:
+        continue
