b204ddf49ad7dbe672659300b5468410cbe8e4a8
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-02-05 21:13:42 +0000

    Update data_extract.py

--- data_extract.py
+++ data_extract.py
@@ -1,9 +1,11 @@
 import wikipedia
 import re
+import pandas as pd
 
 oscar = wikipedia.page('Academy_Awards')
 
 oscar_shows = []
+oscar_edition = []
 
 #collect all the links for the annual oscar shows
 for i in oscar.links:
@@ -13,5 +15,21 @@ for i in oscar.links:
         oscar_shows.append(i)
     elif re.search('.rd Academy Awards', i) is not None:
         oscar_shows.append(i)
+    elif re.search('.nd Academy Awards', i) is not None:
+        oscar_shows.append(i)
     else:
         continue
+        
+#prep the data to be ordered
+for i in oscar_shows:
+    if len(i) == 18:
+        oscar_edition.append(int(i[:1]))
+    elif len(i) == 19:
+        oscar_edition.append(int(i[:2]))
+    else:
+        continue
+
+#create a dataframe of oscar data
+oscars = pd.DataFrame({'oscar_number':oscar_edition,'oscar_show':oscar_shows})   
+#sort the oscar data by edition number 
+oscars = oscars.sort_values('oscar_number')
